d14eaa65fbb21c30704f11f2dc641c61249ac81f
Patric Vormstein <pvormstein@googlemail.com>
2021-06-24 14:03:32 +0200

    remove appdash

--- examples/chat/server/server.go
+++ examples/chat/server/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -11,18 +10,12 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gorilla/websocket"
-	"github.com/opentracing/opentracing-go"
 	"github.com/rs/cors"
-	"sourcegraph.com/sourcegraph/appdash"
-	appdashtracer "sourcegraph.com/sourcegraph/appdash/opentracing"
-	"sourcegraph.com/sourcegraph/appdash/traceapp"
 
 	"github.com/jensneuse/graphql-go-tools/examples/chat"
 )
 
 func main() {
-	startAppdashServer()
-
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3080", "http://localhost:3000"},
 		AllowCredentials: true,
@@ -47,36 +40,4 @@ func main() {
 	log.Println("Playground running on: http://localhost:8085")
 	log.Println("Send operations to: http://localhost:8085/query")
 	log.Fatal(http.ListenAndServe(":8085", nil))
-}
-
-func startAppdashServer() opentracing.Tracer {
-	memStore := appdash.NewMemoryStore()
-	store := &appdash.RecentStore{
-		MinEvictAge: 5 * time.Minute,
-		DeleteStore: memStore,
-	}
-
-	url, err := url.Parse("http://localhost:8700")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tapp, err := traceapp.New(nil, url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tapp.Store = store
-	tapp.Queryer = memStore
-
-	go func() {
-		log.Fatal(http.ListenAndServe(":8700", tapp))
-	}()
-	tapp.Store = store
-	tapp.Queryer = memStore
-
-	collector := appdash.NewLocalCollector(store)
-	tracer := appdashtracer.NewTracer(collector)
-	opentracing.InitGlobalTracer(tracer)
-
-	log.Println("Appdash web UI running on: http://localhost:8700")
-	return tracer
 }
