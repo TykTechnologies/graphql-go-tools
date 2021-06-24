6d2916b2c57073e3097545625e019799c2180925
Patric Vormstein <pvormstein@googlemail.com>
2021-06-24 14:47:00 +0200

    add handler function to chat package

--- examples/chat/server/server.go
+++ examples/chat/server/server.go
@@ -3,13 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 
 	"github.com/jensneuse/graphql-go-tools/examples/chat"
@@ -21,21 +16,8 @@ func main() {
 		AllowCredentials: true,
 	})
 
-	srv := handler.New(chat.NewExecutableSchema(chat.New()))
-
-	srv.AddTransport(transport.POST{})
-	srv.AddTransport(transport.Websocket{
-		KeepAlivePingInterval: 10 * time.Second,
-		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		},
-	})
-	srv.Use(extension.Introspection{})
-
 	http.Handle("/", playground.Handler("Chat", "/query"))
-	http.Handle("/query", c.Handler(srv))
+	http.Handle("/query", c.Handler(chat.GraphQLEndpointHandler()))
 
 	log.Println("Playground running on: http://localhost:8085")
 	log.Println("Send operations to: http://localhost:8085/query")
