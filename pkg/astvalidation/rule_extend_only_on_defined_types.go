c217e4083e8e59a57c4291330a9cb06f4a0fa2c1
Patric Vormstein <pvormstein@googlemail.com>
2021-11-15 14:40:40 +0100

    implement definition checks for extensions

--- pkg/astvalidation/rule_extend_only_on_defined_types.go
+++ pkg/astvalidation/rule_extend_only_on_defined_types.go
@@ -3,15 +3,22 @@ package astvalidation
 import (
 	"github.com/jensneuse/graphql-go-tools/pkg/ast"
 	"github.com/jensneuse/graphql-go-tools/pkg/astvisitor"
+	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
 )
 
 func ExtendOnlyOnDefinedTypes() Rule {
 	return func(walker *astvisitor.Walker) {
-		visitor := &knownTypeNamesVisitor{
+		visitor := &extendOnlyOnDefinedTypesVisitor{
 			Walker: walker,
 		}
 
-		walker.RegisterDocumentVisitor(visitor)
+		walker.RegisterEnterDocumentVisitor(visitor)
+		walker.RegisterEnterScalarTypeExtensionVisitor(visitor)
+		walker.RegisterEnterObjectTypeExtensionVisitor(visitor)
+		walker.RegisterEnterInterfaceTypeExtensionVisitor(visitor)
+		walker.RegisterEnterUnionTypeExtensionVisitor(visitor)
+		walker.RegisterEnterEnumTypeExtensionVisitor(visitor)
+		walker.RegisterEnterInputObjectTypeExtensionVisitor(visitor)
 	}
 }
 
@@ -22,4 +29,61 @@ type extendOnlyOnDefinedTypesVisitor struct {
 
 func (e *extendOnlyOnDefinedTypesVisitor) EnterDocument(operation, definition *ast.Document) {
 	e.definition = operation
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) EnterScalarTypeExtension(ref int) {
+	name := e.definition.ScalarTypeExtensionNameBytes(ref)
+	if !e.extensionIsValidForNodeKind(name, ast.NodeKindScalarTypeDefinition) {
+		e.Report.AddExternalError(operationreport.ErrScalarTypeUndefined(name))
+	}
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) EnterObjectTypeExtension(ref int) {
+	name := e.definition.ObjectTypeExtensionNameBytes(ref)
+	if !e.extensionIsValidForNodeKind(name, ast.NodeKindObjectTypeDefinition) {
+		e.Report.AddExternalError(operationreport.ErrTypeUndefined(name))
+	}
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) EnterInterfaceTypeExtension(ref int) {
+	name := e.definition.InterfaceTypeExtensionNameBytes(ref)
+	if !e.extensionIsValidForNodeKind(name, ast.NodeKindInterfaceTypeDefinition) {
+		e.Report.AddExternalError(operationreport.ErrInterfaceTypeUndefined(name))
+	}
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) EnterUnionTypeExtension(ref int) {
+	name := e.definition.UnionTypeExtensionNameBytes(ref)
+	if !e.extensionIsValidForNodeKind(name, ast.NodeKindUnionTypeDefinition) {
+		e.Report.AddExternalError(operationreport.ErrUnionTypeUndefined(name))
+	}
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) EnterEnumTypeExtension(ref int) {
+	name := e.definition.EnumTypeExtensionNameBytes(ref)
+	if !e.extensionIsValidForNodeKind(name, ast.NodeKindEnumTypeDefinition) {
+		e.Report.AddExternalError(operationreport.ErrEnumTypeUndefined(name))
+	}
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) EnterInputObjectTypeExtension(ref int) {
+	name := e.definition.InputObjectTypeExtensionNameBytes(ref)
+	if !e.extensionIsValidForNodeKind(name, ast.NodeKindInputObjectTypeDefinition) {
+		e.Report.AddExternalError(operationreport.ErrInputObjectTypeUndefined(name))
+	}
+}
+
+func (e *extendOnlyOnDefinedTypesVisitor) extensionIsValidForNodeKind(name ast.ByteSlice, definitionNodeKind ast.NodeKind) bool {
+	nodes, exists := e.definition.Index.NodesByNameBytes(name)
+	if !exists {
+		return true
+	}
+
+	for i := 0; i < len(nodes); i++ {
+		if nodes[i].Kind == definitionNodeKind {
+			return true
+		}
+	}
+
+	return false
 }
