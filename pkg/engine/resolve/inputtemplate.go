2768ffcfd1fa63cb274dca6d2c4485af22bd9b2a
Patric Vormstein <pvormstein@googlemail.com>
2022-05-24 11:03:11 +0200

    fix issue with plain variable renderer

--- pkg/engine/resolve/inputtemplate.go
+++ pkg/engine/resolve/inputtemplate.go
@@ -63,7 +63,9 @@ func (i *InputTemplate) renderObjectVariable(ctx context.Context, variables []by
 		value = variables[offset-len(value)-2 : offset]
 		switch segment.Renderer.GetKind() {
 		case VariableRendererKindPlain:
-			segment.Renderer.(*PlainVariableRenderer).rootValueType.Value = valueType
+			if plainRenderer, ok := (segment.Renderer).(*PlainVariableRenderer); ok {
+				plainRenderer.rootValueType.Value = valueType
+			}
 		}
 	}
 	return segment.Renderer.RenderVariable(ctx, value, preparedInput)
@@ -77,6 +79,12 @@ func (i *InputTemplate) renderContextVariable(ctx *Context, segment TemplateSegm
 	}
 	if valueType == jsonparser.String {
 		value = ctx.Variables[offset-len(value)-2 : offset]
+		switch segment.Renderer.GetKind() {
+		case VariableRendererKindPlain:
+			if plainRenderer, ok := (segment.Renderer).(*PlainVariableRenderer); ok {
+				plainRenderer.rootValueType.Value = valueType
+			}
+		}
 	}
 	return segment.Renderer.RenderVariable(ctx, value, preparedInput)
 }
