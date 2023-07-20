3cc2f561a9f10c768f487aaefce2da282e6040e9
Patric Vormstein <pvormstein@googlemail.com>
2023-07-20 12:59:00 +0200

    implement GraphQLTransportWSMessageWriter

--- pkg/subscription/websocket/protocol_graphql_transport_ws.go
+++ pkg/subscription/websocket/protocol_graphql_transport_ws.go
@@ -2,8 +2,12 @@ package websocket
 
 import (
 	"encoding/json"
+	"sync"
 
 	"github.com/jensneuse/abstractlogger"
+
+	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
+	"github.com/TykTechnologies/graphql-go-tools/pkg/subscription"
 )
 
 type GraphQLTransportWSMessageType string
@@ -20,9 +24,9 @@ const (
 )
 
 type GraphQLTransportWSMessage struct {
-	Id      string                            `json:"id,omitempty"`
-	Type    GraphQLTransportWSMessageType     `json:"type"`
-	Payload *GraphQLTransportWSMessagePayload `json:"payload,omitempty"`
+	Id      string                        `json:"id,omitempty"`
+	Type    GraphQLTransportWSMessageType `json:"type"`
+	Payload json.RawMessage               `json:"payload,omitempty"`
 }
 
 type GraphQLTransportWSMessagePayload struct {
@@ -49,4 +53,100 @@ func (g *GraphQLTransportWSMessageReader) Read(data []byte) (*GraphQLTransportWS
 		return nil, err
 	}
 	return &message, nil
+}
+
+func (g *GraphQLTransportWSMessageReader) DeserializeSubscribePayload(message *GraphQLTransportWSMessage) (*GraphQLTransportWSMessagePayload, error) {
+	var deserializedPayload GraphQLTransportWSMessagePayload
+	err := json.Unmarshal(message.Payload, &deserializedPayload)
+	if err != nil {
+		g.logger.Error("websocket.GraphQLTransportWSMessageReader.DeserializeSubscribePayload: on subscribe payload deserialization",
+			abstractlogger.Error(err),
+			abstractlogger.ByteString("payload", message.Payload),
+		)
+		return nil, err
+	}
+
+	return &deserializedPayload, nil
+}
+
+// GraphQLTransportWSMessageWriter can be used to write graphql-transport-ws messages to a transport client.
+type GraphQLTransportWSMessageWriter struct {
+	logger abstractlogger.Logger
+	client subscription.TransportClient
+	mu     *sync.Mutex
+}
+
+// WriteConnectionAck writes a message of type 'connection_ack' to the transport client.
+func (g *GraphQLTransportWSMessageWriter) WriteConnectionAck() error {
+	message := &GraphQLTransportWSMessage{
+		Type: GraphQLTransportWSMessageTypeConnectionAck,
+	}
+	return g.write(message)
+}
+
+// WritePing writes a message of type 'ping' to the transport client. Payload is optional.
+func (g *GraphQLTransportWSMessageWriter) WritePing(payload []byte) error {
+	message := &GraphQLTransportWSMessage{
+		Type:    GraphQLTransportWSMessageTypePing,
+		Payload: payload,
+	}
+	return g.write(message)
+}
+
+// WritePong writes a message of type 'pong' to the transport client. Payload is optional.
+func (g *GraphQLTransportWSMessageWriter) WritePong(payload []byte) error {
+	message := &GraphQLTransportWSMessage{
+		Type:    GraphQLTransportWSMessageTypePong,
+		Payload: payload,
+	}
+	return g.write(message)
+}
+
+// WriteNext writes a message of type 'next' to the transport client including the execution result as payload.
+func (g *GraphQLTransportWSMessageWriter) WriteNext(id string, executionResult []byte) error {
+	message := &GraphQLTransportWSMessage{
+		Id:      id,
+		Type:    GraphQLTransportWSMessageTypeNext,
+		Payload: executionResult,
+	}
+	return g.write(message)
+}
+
+// WriteError writes a message of type 'error' to the transport client including the graphql errors as payload.
+func (g *GraphQLTransportWSMessageWriter) WriteError(id string, graphqlErrors graphql.RequestErrors) error {
+	payloadBytes, err := json.Marshal(graphqlErrors)
+	if err != nil {
+		return err
+	}
+	message := &GraphQLTransportWSMessage{
+		Id:      id,
+		Type:    GraphQLTransportWSMessageTypeError,
+		Payload: payloadBytes,
+	}
+	return g.write(message)
+}
+
+// WriteComplete writes a message of type 'complete' to the transport client.
+func (g *GraphQLTransportWSMessageWriter) WriteComplete(id string) error {
+	message := &GraphQLTransportWSMessage{
+		Id:   id,
+		Type: GraphQLTransportWSMessageTypeComplete,
+	}
+	return g.write(message)
+}
+
+func (g *GraphQLTransportWSMessageWriter) write(message *GraphQLTransportWSMessage) error {
+	jsonData, err := json.Marshal(message)
+	if err != nil {
+		g.logger.Error("websocket.GraphQLTransportWSMessageWriter.write: on json marshal",
+			abstractlogger.Error(err),
+			abstractlogger.String("id", message.Id),
+			abstractlogger.String("type", string(message.Type)),
+			abstractlogger.Any("payload", message.Payload),
+		)
+		return err
+	}
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	return g.client.WriteBytesToClient(jsonData)
 }
--- pkg/subscription/websocket/protocol_graphql_transport_ws_test.go
+++ pkg/subscription/websocket/protocol_graphql_transport_ws_test.go
@@ -1,10 +1,14 @@
 package websocket
 
 import (
+	"errors"
+	"sync"
 	"testing"
 
 	"github.com/jensneuse/abstractlogger"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
 )
 
 func TestGraphQLTransportWSMessageReader_Read(t *testing.T) {
@@ -22,15 +26,12 @@ func TestGraphQLTransportWSMessageReader_Read(t *testing.T) {
 		assert.Equal(t, expectedMessage, message)
 	})
 
-	t.Run("should read a message without variables and extensions", func(t *testing.T) {
-		data := []byte(`{ "id": "1", "type": "subscribe", "payload": { "operationName": "MyQuery", "query": "query MyQuery { hello }" } }`)
+	t.Run("should message with json payload", func(t *testing.T) {
+		data := []byte(`{ "id": "1", "type": "connection_init", "payload": { "Authorization": "Bearer ey123" } }`)
 		expectedMessage := &GraphQLTransportWSMessage{
-			Id:   "1",
-			Type: "subscribe",
-			Payload: &GraphQLTransportWSMessagePayload{
-				OperationName: "MyQuery",
-				Query:         "query MyQuery { hello }",
-			},
+			Id:      "1",
+			Type:    "connection_init",
+			Payload: []byte(`{ "Authorization": "Bearer ey123" }`),
 		}
 
 		reader := GraphQLTransportWSMessageReader{
@@ -41,7 +42,7 @@ func TestGraphQLTransportWSMessageReader_Read(t *testing.T) {
 		assert.Equal(t, expectedMessage, message)
 	})
 
-	t.Run("should read complete message", func(t *testing.T) {
+	t.Run("should read and deserialize subscribe message", func(t *testing.T) {
 		data := []byte(`{ 
   "id": "1", 
   "type": "subscribe", 
@@ -55,12 +56,12 @@ func TestGraphQLTransportWSMessageReader_Read(t *testing.T) {
 		expectedMessage := &GraphQLTransportWSMessage{
 			Id:   "1",
 			Type: "subscribe",
-			Payload: &GraphQLTransportWSMessagePayload{
-				OperationName: "MyQuery",
-				Query:         "query MyQuery($name: String) { hello(name: $name) }",
-				Variables:     []byte(`{ "name": "Udo" }`),
-				Extensions:    []byte(`{ "Authorization": "Bearer ey123" }`),
-			},
+			Payload: []byte(`{ 
+    "operationName": "MyQuery", 
+    "query": "query MyQuery($name: String) { hello(name: $name) }", 
+    "variables": { "name": "Udo" },
+    "extensions": { "Authorization": "Bearer ey123" }
+  }`),
 		}
 
 		reader := GraphQLTransportWSMessageReader{
@@ -69,5 +70,190 @@ func TestGraphQLTransportWSMessageReader_Read(t *testing.T) {
 		message, err := reader.Read(data)
 		assert.NoError(t, err)
 		assert.Equal(t, expectedMessage, message)
+
+		expectedPayload := &GraphQLTransportWSMessagePayload{
+			OperationName: "MyQuery",
+			Query:         "query MyQuery($name: String) { hello(name: $name) }",
+			Variables:     []byte(`{ "name": "Udo" }`),
+			Extensions:    []byte(`{ "Authorization": "Bearer ey123" }`),
+		}
+		actualPayload, err := reader.DeserializeSubscribePayload(message)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedPayload, actualPayload)
+	})
+}
+
+func TestGraphQLTransportWSMessageWriter_WriteConnectionAck(t *testing.T) {
+	t.Run("should return error when error occurs on underlying call", func(t *testing.T) {
+		testClient := NewTestClient(true)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		err := writer.WriteConnectionAck()
+		assert.Error(t, err)
+	})
+	t.Run("should successfully write ack message to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"type":"connection_ack"}`)
+		err := writer.WriteConnectionAck()
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+}
+
+func TestGraphQLTransportWSMessageWriter_WritePing(t *testing.T) {
+	t.Run("should return error when error occurs on underlying call", func(t *testing.T) {
+		testClient := NewTestClient(true)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		err := writer.WritePing(nil)
+		assert.Error(t, err)
+	})
+	t.Run("should successfully write ping message to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"type":"ping"}`)
+		err := writer.WritePing(nil)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+	t.Run("should successfully write ping message with payload to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"type":"ping","payload":{"connected_since":"10min"}}`)
+		err := writer.WritePing([]byte(`{"connected_since":"10min"}`))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+}
+
+func TestGraphQLTransportWSMessageWriter_WritePong(t *testing.T) {
+	t.Run("should return error when error occurs on underlying call", func(t *testing.T) {
+		testClient := NewTestClient(true)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		err := writer.WritePong(nil)
+		assert.Error(t, err)
+	})
+	t.Run("should successfully write pong message to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"type":"pong"}`)
+		err := writer.WritePong(nil)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+	t.Run("should successfully write pong message with payload to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"type":"pong","payload":{"connected_since":"10min"}}`)
+		err := writer.WritePong([]byte(`{"connected_since":"10min"}`))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+}
+
+func TestGraphQLTransportWSMessageWriter_WriteNext(t *testing.T) {
+	t.Run("should return error when error occurs on underlying call", func(t *testing.T) {
+		testClient := NewTestClient(true)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		err := writer.WriteNext("1", nil)
+		assert.Error(t, err)
+	})
+	t.Run("should successfully write next message with payload to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"id":"1","type":"next","payload":{"data":{"hello":"world"}}}`)
+		err := writer.WriteNext("1", []byte(`{"data":{"hello":"world"}}`))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+}
+
+func TestGraphQLTransportWSMessageWriter_WriteError(t *testing.T) {
+	t.Run("should return error when error occurs on underlying call", func(t *testing.T) {
+		testClient := NewTestClient(true)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		err := writer.WriteError("1", nil)
+		assert.Error(t, err)
+	})
+	t.Run("should successfully write error message with payload to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"id":"1","type":"error","payload":[{"message":"request error"}]}`)
+		requestErrors := graphql.RequestErrorsFromError(errors.New("request error"))
+		err := writer.WriteError("1", requestErrors)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
+	})
+}
+
+func TestGraphQLTransportWSMessageWriter_WriteComplete(t *testing.T) {
+	t.Run("should return error when error occurs on underlying call", func(t *testing.T) {
+		testClient := NewTestClient(true)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		err := writer.WriteComplete("1")
+		assert.Error(t, err)
+	})
+	t.Run("should successfully write complete message to client", func(t *testing.T) {
+		testClient := NewTestClient(false)
+		writer := GraphQLTransportWSMessageWriter{
+			logger: abstractlogger.Noop{},
+			client: testClient,
+			mu:     &sync.Mutex{},
+		}
+		expectedMessage := []byte(`{"id":"1","type":"complete"}`)
+		err := writer.WriteComplete("1")
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessage, testClient.readMessageToClient())
 	})
 }
