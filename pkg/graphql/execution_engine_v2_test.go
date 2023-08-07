4c0bbdf17da343712a08a83f9ac5713b8df9c861
Burak Sezer <burak.sezer.developer@gmail.com>
2023-08-07 10:30:21 +0300

    [TT-9257] Use the correct version of execution_engine_v2_test.go

--- pkg/graphql/execution_engine_v2_test.go
+++ pkg/graphql/execution_engine_v2_test.go
@@ -12,6 +12,7 @@ import (
 	"net/http/httptest"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/jensneuse/abstractlogger"
 	"github.com/stretchr/testify/assert"
@@ -1831,6 +1832,156 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 			expectedResponse: `{"data":{"searchResults":null}}`,
 		},
 	))
+}
+
+func TestExecutionEngineV2_FederationAndSubscription_IntegrationTest(t *testing.T) {
+
+	runIntegration := func(t *testing.T, enableDataLoader bool, secondRun bool) {
+		t.Helper()
+		ctx, cancelFn := context.WithCancel(context.Background())
+		setup := newFederationSetup()
+		t.Cleanup(func() {
+			cancelFn()
+			setup.accountsUpstreamServer.Close()
+			setup.productsUpstreamServer.Close()
+			setup.reviewsUpstreamServer.Close()
+			setup.pollingUpstreamServer.Close()
+		})
+
+		engine, schema, err := newFederationEngine(ctx, setup, enableDataLoader)
+		require.NoError(t, err)
+
+		t.Run("should successfully execute a federation operation", func(t *testing.T) {
+			gqlRequest := &Request{
+				OperationName: "",
+				Variables:     nil,
+				Query:         federationtesting.QueryReviewsOfMe,
+			}
+
+			validationResult, err := gqlRequest.ValidateForSchema(schema)
+			require.NoError(t, err)
+			require.True(t, validationResult.Valid)
+
+			execCtx, execCtxCancelFn := context.WithCancel(context.Background())
+			defer execCtxCancelFn()
+
+			resultWriter := NewEngineResultWriter()
+			err = engine.Execute(execCtx, gqlRequest, &resultWriter)
+			if assert.NoError(t, err) {
+				assert.Equal(t,
+					`{"data":{"me":{"reviews":[{"body":"A highly effective form of birth control.","product":{"upc":"top-1","name":"Trilby","price":11}},{"body":"Fedoras are one of the most fashionable hats around and can look great with a variety of outfits.","product":{"upc":"top-2","name":"Fedora","price":22}}]}}}`,
+					resultWriter.String(),
+				)
+			}
+		})
+
+		t.Run("should successfully execute a federation subscription", func(t *testing.T) {
+			query := `
+subscription UpdatedPrice {
+  updatedPrice {
+    name
+    price
+	reviews {
+      body
+      author {
+		id
+		username
+      }
+    }
+  }
+}`
+
+			gqlRequest := &Request{
+				OperationName: "",
+				Variables:     nil,
+				Query:         query,
+			}
+
+			validationResult, err := gqlRequest.ValidateForSchema(schema)
+			require.NoError(t, err)
+			require.True(t, validationResult.Valid)
+
+			execCtx, execCtxCancelFn := context.WithCancel(context.Background())
+			defer execCtxCancelFn()
+
+			message := make(chan string)
+			resultWriter := NewEngineResultWriter()
+			resultWriter.SetFlushCallback(func(data []byte) {
+				message <- string(data)
+			})
+
+			go func() {
+				err := engine.Execute(execCtx, gqlRequest, &resultWriter)
+				assert.NoError(t, err)
+			}()
+
+			if assert.NoError(t, err) {
+				assert.Eventuallyf(t, func() bool {
+					msg := `{"data":{"updatedPrice":{"name":"Trilby","price":%d,"reviews":[{"body":"A highly effective form of birth control.","author":{"id":"1234","username":"Me"}}]}}}`
+					price := 10
+					if secondRun {
+						price += 2
+					}
+
+					firstMessage := <-message
+					expectedFirstMessage := fmt.Sprintf(msg, price)
+					assert.Equal(t, expectedFirstMessage, firstMessage)
+
+					secondMessage := <-message
+					expectedSecondMessage := fmt.Sprintf(msg, price+1)
+					assert.Equal(t, expectedSecondMessage, secondMessage)
+					return true
+				}, time.Second, 10*time.Millisecond, "did not receive expected messages")
+			}
+		})
+
+		/* Uncomment when polling subscriptions are ready:
+
+		t.Run("should successfully subscribe to rest data source", func(t *testing.T) {
+			gqlRequest := &Request{
+				OperationName: "",
+				Variables:     nil,
+				Query:         "subscription Counter { counter }",
+			}
+
+			validationResult, err := gqlRequest.ValidateForSchema(setup.schema)
+			require.NoError(t, err)
+			require.True(t, validationResult.Valid)
+
+			execCtx, execCtxCancelFn := context.WithCancel(context.Background())
+			defer execCtxCancelFn()
+
+			message := make(chan string)
+			resultWriter := NewEngineResultWriter()
+			resultWriter.SetFlushCallback(func(data []byte) {
+				fmt.Println(string(data))
+				message <- string(data)
+			})
+
+			err = setup.engine.Execute(execCtx, gqlRequest, &resultWriter)
+			assert.NoError(t, err)
+
+			if assert.NoError(t, err) {
+				assert.Eventuallyf(t, func() bool {
+					firstMessage := <-message
+					assert.Equal(t, `{"data":{"counter":1}}`, firstMessage)
+					secondMessage := <-message
+					assert.Equal(t, `{"data":{"counter":2}}`, secondMessage)
+					return true
+				}, time.Second, 10*time.Millisecond, "did not receive expected messages")
+			}
+		})
+		*/
+
+	}
+
+	t.Run("federation", func(t *testing.T) {
+		runIntegration(t, false, false)
+	})
+
+	t.Run("federation with data loader enabled", func(t *testing.T) {
+		runIntegration(t, true, true)
+	})
 }
 
 func testNetHttpClient(t *testing.T, testCase roundTripperTestCase) *http.Client {
