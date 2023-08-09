b639119ac4ff87a562791ae928f547e05b2f7f21
Patric Vormstein <pvormstein@googlemail.com>
2023-08-09 11:26:30 +0200

    move TimeOutChecker into its own file

--- pkg/subscription/handler.go
+++ pkg/subscription/handler.go
@@ -198,29 +198,3 @@ func (u *UniversalProtocolHandler) Handle(ctx context.Context) {
 		}
 	}
 }
-
-type TimeOutParams struct {
-	Name            string
-	Logger          abstractlogger.Logger
-	TimeOutContext  context.Context
-	TimeOutAction   func()
-	TimeOutDuration time.Duration
-}
-
-func TimeOutChecker(params TimeOutParams) {
-	timer := time.NewTimer(params.TimeOutDuration)
-	defer timer.Stop()
-
-	for {
-		select {
-		case <-params.TimeOutContext.Done():
-			return
-		case <-timer.C:
-			params.Logger.Error("time out happened",
-				abstractlogger.String("name", params.Name),
-			)
-			params.TimeOutAction()
-			return
-		}
-	}
-}
