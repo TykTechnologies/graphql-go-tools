5c44551e58d5c78bb3f1f0ef959400f73f5f6111
Kofo Okesola <okesolakofo@gmail.com>
2023-08-30 12:42:52 +0100

    fix mock broker related test failures

--- pkg/engine/datasource/kafka_datasource/kafka_consumer_group_test.go
+++ pkg/engine/datasource/kafka_datasource/kafka_consumer_group_test.go
@@ -18,7 +18,7 @@ import (
 const defaultPartition = 0
 
 // newMockKafkaBroker creates a MockBroker to test ConsumerGroups.
-func newMockKafkaBroker(t *testing.T, topic, group string, fr *sarama.FetchResponse) *sarama.MockBroker {
+func newMockKafkaBroker(t *testing.T, topic, group string, fr *sarama.MockFetchResponse) *sarama.MockBroker {
 	mockBroker := sarama.NewMockBroker(t, 0)
 
 	mockMetadataResponse := sarama.NewMockMetadataResponse(t).
@@ -54,63 +54,7 @@ func newMockKafkaBroker(t *testing.T, topic, group string, fr *sarama.FetchRespo
 
 	// Need to mock ApiVersionsRequest when we upgrade Sarama
 
-	//mockApiVersionsResponse := sarama.NewMockApiVersionsResponse(t)
-	mockOffsetCommitResponse := sarama.NewMockOffsetCommitResponse(t)
-	mockBroker.SetHandlerByMap(map[string]sarama.MockResponse{
-		"MetadataRequest":        mockMetadataResponse,
-		"ProduceRequest":         mockProducerResponse,
-		"OffsetRequest":          mockOffsetResponse,
-		"OffsetFetchRequest":     mockOffsetFetchResponse,
-		"FetchRequest":           sarama.NewMockSequence(sarama.NewMockFetchResponse(t, 1).SetMessage(topic, defaultPartition, 0, sarama.StringEncoder("test"))),
-		"FindCoordinatorRequest": mockCoordinatorResponse,
-		"JoinGroupRequest":       mockJoinGroupResponse,
-		"SyncGroupRequest":       mockSyncGroupResponse,
-		"HeartbeatRequest":       mockHeartbeatResponse,
-		//"ApiVersionsRequest":     mockApiVersionsResponse,
-		"OffsetCommitRequest": mockOffsetCommitResponse,
-	})
-
-	return mockBroker
-}
-
-// newMockKafkaBroker creates a MockBroker to test ConsumerGroups.
-func mNewMockKafkaBroker(t *testing.T, topic, group string, fr *sarama.MockFetchResponse) *sarama.MockBroker {
-	mockBroker := sarama.NewMockBroker(t, 0)
-
-	mockMetadataResponse := sarama.NewMockMetadataResponse(t).
-		SetBroker(mockBroker.Addr(), mockBroker.BrokerID()).
-		SetLeader(topic, defaultPartition, mockBroker.BrokerID()).
-		SetController(mockBroker.BrokerID())
-
-	mockProducerResponse := sarama.NewMockProduceResponse(t).
-		SetError(topic, 0, sarama.ErrNoError).
-		SetVersion(2)
-
-	mockOffsetResponse := sarama.NewMockOffsetResponse(t).
-		SetOffset(topic, defaultPartition, sarama.OffsetOldest, 0).
-		SetOffset(topic, defaultPartition, sarama.OffsetNewest, 1)
-
-	mockCoordinatorResponse := sarama.NewMockFindCoordinatorResponse(t).
-		SetCoordinator(sarama.CoordinatorType(0), group, mockBroker)
-
-	// TODO fix rebalance strategy here
-	mockJoinGroupResponse := sarama.NewMockJoinGroupResponse(t).SetGroupProtocol(sarama.RangeBalanceStrategyName)
-
-	mockSyncGroupResponse := sarama.NewMockSyncGroupResponse(t).
-		SetMemberAssignment(&sarama.ConsumerGroupMemberAssignment{
-			Version:  0,
-			Topics:   map[string][]int32{topic: {0}},
-			UserData: nil,
-		})
-
-	mockHeartbeatResponse := sarama.NewMockHeartbeatResponse(t)
-
-	mockOffsetFetchResponse := sarama.NewMockOffsetFetchResponse(t).
-		SetOffset(group, topic, defaultPartition, 0, "", sarama.KError(0))
-
-	// Need to mock ApiVersionsRequest when we upgrade Sarama
-
-	//mockApiVersionsResponse := sarama.NewMockApiVersionsResponse(t)
+	mockApiVersionsResponse := sarama.NewMockApiVersionsResponse(t)
 	mockOffsetCommitResponse := sarama.NewMockOffsetCommitResponse(t)
 	mockBroker.SetHandlerByMap(map[string]sarama.MockResponse{
 		"MetadataRequest":        mockMetadataResponse,
@@ -122,8 +66,8 @@ func mNewMockKafkaBroker(t *testing.T, topic, group string, fr *sarama.MockFetch
 		"JoinGroupRequest":       mockJoinGroupResponse,
 		"SyncGroupRequest":       mockSyncGroupResponse,
 		"HeartbeatRequest":       mockHeartbeatResponse,
-		//"ApiVersionsRequest":     mockApiVersionsResponse,
-		"OffsetCommitRequest": mockOffsetCommitResponse,
+		"ApiVersionsRequest":     mockApiVersionsResponse,
+		"OffsetCommitRequest":    mockOffsetCommitResponse,
 	})
 
 	return mockBroker
@@ -164,7 +108,7 @@ func (d *testConsumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession
 
 func newTestConsumerGroup(groupID string, brokers []string) (sarama.ConsumerGroup, error) {
 	kConfig := mocks.NewTestConfig()
-	kConfig.Version = sarama.DefaultVersion
+	kConfig.Version = SaramaSupportedKafkaVersions[testMockKafkaVersion]
 	kConfig.Consumer.Return.Errors = true
 	kConfig.ClientID = "graphql-go-tools-test"
 	kConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
@@ -181,7 +125,7 @@ func TestKafkaMockBroker(t *testing.T) {
 	)
 
 	fr := sarama.NewMockFetchResponse(t, 1)
-	mockBroker := mNewMockKafkaBroker(t, topic, consumerGroup, fr)
+	mockBroker := newMockKafkaBroker(t, topic, consumerGroup, fr)
 	defer mockBroker.Close()
 
 	brokerAddr := []string{mockBroker.Addr()}
@@ -261,12 +205,12 @@ func TestKafkaConsumerGroup_StartConsuming_And_Stop(t *testing.T) {
 		consumerGroup    = "consumer.group"
 	)
 
-	fr := &sarama.FetchResponse{Version: 11}
+	fr := sarama.NewMockFetchResponse(t, 1)
 	mockBroker := newMockKafkaBroker(t, topic, consumerGroup, fr)
 	defer mockBroker.Close()
 
 	// Add a message to the topic. The consumer group will fetch that message and trigger ConsumeClaim method.
-	fr.AddMessage(topic, defaultPartition, testMessageKey, testMessageValue, 0)
+	fr.SetMessageWithKey(topic, defaultPartition, 0, testMessageKey, testMessageValue)
 
 	options := GraphQLSubscriptionOptions{
 		BrokerAddresses: []string{mockBroker.Addr()},
@@ -278,7 +222,7 @@ func TestKafkaConsumerGroup_StartConsuming_And_Stop(t *testing.T) {
 	options.Sanitize()
 	require.NoError(t, options.Validate())
 
-	saramaConfig := sarama.NewConfig()
+	saramaConfig := mocks.NewTestConfig()
 	saramaConfig.Version = SaramaSupportedKafkaVersions[options.KafkaVersion]
 
 	cg, err := NewKafkaConsumerGroup(logger(), saramaConfig, &options)
