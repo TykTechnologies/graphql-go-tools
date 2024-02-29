f0109d3e17d1081ebb55381d441a9132cd8f01da
Patric Vormstein <pvormstein@googlemail.com>
2024-02-29 13:42:35 +0100

    fix handling null default values for input fields with object or list types (#707)

--- v2/pkg/astnormalization/inject_input_default_values.go
+++ v2/pkg/astnormalization/inject_input_default_values.go
@@ -171,13 +171,17 @@ func (v *inputFieldDefaultInjectionVisitor) processObjectOrListInput(fieldType i
 	finalVal := defaultValue
 	replaced := false
 	valIsList := valType == jsonparser.Array
-	if fieldIsList && valIsList {
+
+	switch {
+	case valType == jsonparser.Null:
+		replaced = true
+	case fieldIsList && valIsList:
 		_, err := jsonparser.ArrayEach(varVal, v.jsonWalker(typeDoc.ResolveListOrNameType(fieldType), defaultValue, &node, typeDoc, &finalVal, &replaced))
 		if err != nil {
 			return nil, false, err
 
 		}
-	} else if !fieldIsList && !valIsList {
+	case !fieldIsList && !valIsList:
 		finalVal, replaced, err = v.recursiveInjectInputFields(node.Ref, defaultValue)
 		if err != nil {
 			return nil, false, err
